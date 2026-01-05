e414ce4165644fc163b2dc33df468b250820e67a
Alexander Zhipa <alex.zhipa@proton.me>
2026-01-05 13:37:11 -0500

    feat: add validation metrics logging callback

--- fkat/pytorch/callbacks/logging/__init__.py
+++ fkat/pytorch/callbacks/logging/__init__.py
@@ -2,9 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 from .heartbeat import Heartbeat
 from .throughput import Throughput
+from .validation_metrics import ValidationMetrics
 
 
 __all__ = [
     "Heartbeat",
     "Throughput",
+    "ValidationMetrics",
 ]
